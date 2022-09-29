89a0f36158b15ed8bc6f77a6a52b2f8a86bb3ad1
Pamela Lima <pameladelima@gmail.com>
2022-09-29 20:24:22 -0300

    Material-UI

--- Front-End_React/my-app/src/App.tsx
+++ Front-End_React/my-app/src/App.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import Home from'./páginas/home/Home';
 import './páginas/home/Home.css';
+import Navbar from './components/estaticos/navbar/Navbar';
+import Footer from './components/estaticos/footer/Footer';
 
 function App() {
   return (
-    <Home/>
+    <>
+     <Navbar />
+     <Home />
+     <Footer />
+    </>    
   );
-} 
+}
 export default App;
+
